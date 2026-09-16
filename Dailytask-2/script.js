9dbde3f10ffb538706af84969e6b2daea791805e
Tamil Selvan S <tamil.selvan.75457@gmail.com>
2026-09-16 17:25:05 +0530

    Add files via upload

---
+++ Dailytask-2/script.js
@@ -0,0 +1,12 @@
+let nam=document.getElementById("name");
+let dept=document.getElementById("dept");
+let nchange=document.getElementById("namechange");
+let dchange=document.getElementById("changedept");
+nchange.addEventListener("click",function()
+{
+nam.textContent="Dhanuuu";
+});
+dchange.addEventListener("click",function()
+{
+    dept.textContent="Fullstack Developer";
+});
